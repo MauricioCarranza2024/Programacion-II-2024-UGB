5d7d1b0072d60d988ddec88bd9f69d535f60b465
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-02-13 16:18:54 -0600

    creacion de conversores, y la app de medir

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -14,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
     TabHost tbh;
     TextView tempval;
     Spinner spn;
-    Button btn;
+    Button btnLongitud;
+    Button btnVolumen; // Botón para la conversión de volumen
     conversores miObj = new conversores();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,58 +27,78 @@ public class MainActivity extends AppCompatActivity {
         tbh.setup();
 
         tbh.addTab(tbh.newTabSpec("LON").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("ALM").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null));
-        tbh.addTab(tbh.newTabSpec("MON").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
+        tbh.addTab(tbh.newTabSpec("VOL").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null));
+        tbh.addTab(tbh.newTabSpec("MAS").setContent(R.id.tabMasa).setIndicator("MASA", null));
 
-
-        btn=findViewById(R.id.btnConvertirLongitud);
-        btn.setOnClickListener(new View.OnClickListener() {
+        // Botón para conversión de longitud
+        btnLongitud = findViewById(R.id.btnConvertirLongitud);
+        btnLongitud.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Lógica de conversión de longitud
                 spn = findViewById(R.id.spnDElongitud);
                 int de = spn.getSelectedItemPosition();
 
                 spn = findViewById(R.id.spnAlongitud);
                 int a = spn.getSelectedItemPosition();
 
-                tempval=findViewById(R.id.txtCantidadLongitud);
-                double cantidad= Double.parseDouble(tempval.getText().toString());
+                tempval = findViewById(R.id.txtCantidadLongitud);
+                double cantidad = Double.parseDouble(tempval.getText().toString());
                 double resp = miObj.convertir(0, de, a, cantidad);
-                Toast.makeText(getApplicationContext(),"Respuesta:"+ resp, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
             }
-
         });
-        btn=findViewById(R.id.btnConvertirVolumen);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        // Botón para conversión de volumen
+        btnVolumen = findViewById(R.id.btnConvertirVolumen);
+        btnVolumen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Lógica de conversión de volumen
                 spn = findViewById(R.id.spnDEVolumen);
                 int de = spn.getSelectedItemPosition();
 
                 spn = findViewById(R.id.spnAVolumen);
                 int a = spn.getSelectedItemPosition();
 
-                tempval=findViewById(R.id.txtCantidadDeVolumen);
-                double cantidad= Double.parseDouble(tempval.getText().toString());
-                double resp = miObj.convertir(1, de, a, cantidad);
-                Toast.makeText(getApplicationContext(),"Respuesta:"+ resp, Toast.LENGTH_LONG).show();
+                tempval = findViewById(R.id.txtCantidadDeVolumen);
+                double cantidad = Double.parseDouble(tempval.getText().toString());
+                double resp = miObj.convertir(1, de, a, cantidad); // Se pasa 1 como opción para la conversión de volumen
+                Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
+            }
+        });
+        btnLongitud = findViewById(R.id.btnConvertirMasa);
+        btnLongitud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lógica de conversión de longitud
+                spn = findViewById(R.id.spnDEMasa);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAMasa);
+                int a = spn.getSelectedItemPosition();
+
+                tempval = findViewById(R.id.txtCantidadDeMasa);
+                double cantidad = Double.parseDouble(tempval.getText().toString());
+                double resp = miObj.convertir(2, de, a, cantidad);
+                Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
             }
         });
     }
 }
+
 class conversores {
     double[][] valores = {
+            // Longitud
             {1, 100, 39.3701, 3.28084, 1.193, 1.09361, 0.001, 0.000621371},
-            {1},
-            {1},
-
-            {1, 1000}, // 1 litro = 1000 mililitros
+            // Volumen
+            {1, 1000, 0.001},
+            // Masa
+            {1, 1000, 0.00220462, 0.035274, 0.001, 1e+6, 9.8421e-7, 1.1023e-6},
     };
-
-
-
 
     public double convertir(int opcion, int de, int a, double cantidad) {
         return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
+
