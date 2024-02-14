6805b4fc47c72c4f1bbb06337e698972ee2898fd
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-02-13 22:44:32 -0600

    creacion de conversores, y la app de medir

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnVolumen;
     Button btnMasa;
     Button btnAlmacenamiento;
+    Button btnTransmision;
     conversores miObj = new conversores();
 
     @Override
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         tbh.addTab(tbh.newTabSpec("VOL").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null));
         tbh.addTab(tbh.newTabSpec("MAS").setContent(R.id.tabMasa).setIndicator("MASA", null));
         tbh.addTab(tbh.newTabSpec("ALM").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("TRS").setContent(R.id.tabTransmision).setIndicator("TRANSMISION", null));
 
         // Botón para conversión de longitud
         btnLongitud = findViewById(R.id.btnConvertirLongitud);
@@ -103,6 +105,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
             }
         });
+        btnTransmision = findViewById(R.id.btnConvertirTransmision);
+        btnTransmision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                spn = findViewById(R.id.spnDETransmision);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnATransmision);
+                int a = spn.getSelectedItemPosition();
+
+                tempval = findViewById(R.id.txtCantidadDeTransmision);
+                double cantidad = Double.parseDouble(tempval.getText().toString());
+                double resp = miObj.convertir(4, de, a, cantidad);
+                Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
 
@@ -111,11 +130,13 @@ class conversores {
             // Longitud
             {1, 100, 39.3701, 3.28084, 1.193, 1.09361, 0.001, 0.000621371},
             // Volumen
-            {1, 1000, 0.001, 0},  // Agrega un 0 para completar la matriz
+            {1, 1000, 0.001, 0},
             // Masa
             {1, 1000, 0.00220462, 0.035274, 0.001, 1e+6, 9.8421e-7, 1.1023e-6},
             // Almacenamiento
             {1, 1e-6, 1e-9, 0.125, 0.000125, 1.25e-7, 1.25e-10, 1.25e-13},
+            //transmision
+            {1, 0.001, 1e-6}
     };
 
     public double convertir(int opcion, int de, int a, double cantidad) {
